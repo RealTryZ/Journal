11f310009d0f3e468bf2ea91dab4730b0eee1d67
RealTryZ <mcwitti.pw@gmail.com>
2026-01-27 21:52:21 +0100

    * German, App Icon, Fix Language

--- app/src/main/java/io/github/realtryz/journal/MainActivity.kt
+++ app/src/main/java/io/github/realtryz/journal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -79,7 +80,7 @@ val journalColorResources = listOf(
     R.color.purple_700
 )
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
